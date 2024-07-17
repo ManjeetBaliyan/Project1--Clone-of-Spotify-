0412e6d4581b73ca5a04dc14d86024cfec21fbb5
ManjeetBaliyan <manjeetbaliyan21@gmail.com>
2024-07-17 23:27:46 +0530

    delete

--- script.js
+++
@@ -1,6 +0,0 @@
-async function main() {
-  let a = await fetch("http://127.0.0.1:5500/songs/Big Sky Elegy - National Sweetheart.mp3")
-  let response = await a.text();
-  console.log(response);
-}
-main()
