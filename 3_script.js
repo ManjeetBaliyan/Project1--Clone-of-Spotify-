d919eb816d093e82d1647f5f693da3639ecd3317
Sumil Verma <verma.sumil257@gmail.com>
2024-07-18 15:32:22 +0530

    added song play functionality

--- 3_script.js
+++ 3_script.js
@@ -1,6 +1,46 @@
-async function main() {
-  let a = await fetch("http://127.0.0.1:5500/songs/Big Sky Elegy - National Sweetheart.mp3")
-  let response = await a.text();
-  console.log(response);
-}
-main()
+let songIndex = 0;
+let songItems = Array.from(document.getElementsByClassName("card"));
+let audioElement = new Audio('/songs/1.mp3')
+
+let songs = [
+  {songName : "",
+    filePath : "./songs/1.mp3"
+  },
+  {songName : "",
+    filePath : "./songs/2.mp3"
+  },
+  {songName : "",
+    filePath : "./songs/3.mp3"
+  },
+  {songName : "",
+    filePath : "./songs/4.mp3"
+  },
+  {songName : "",
+    filePath : "./songs/5.mp3"
+  },
+  {songName : "",
+    filePath : "./songs/6.mp3"
+  },
+  {songName : "",
+    filePath : "./songs/7.mp3"
+  }
+];
+
+const playSong = () => {
+  audioElement.src = songs[songIndex].filePath;
+  audioElement.currentTime = 0;
+  audioElement.play();
+};
+
+const initializeSongItems = () => {
+  songItems.forEach((element, i) => {
+    element
+      .getElementsByClassName("play")[0]
+      .addEventListener("click", (e) => {
+        songIndex = i;
+        playSong();
+      });
+  });
+};
+
+initializeSongItems();
